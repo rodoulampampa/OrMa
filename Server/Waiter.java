a45a1710dea8049f77fe7c6368f93fb77661a357
rodoulampampa <32978419+rodoulampampa@users.noreply.github.com>
2019-05-07 21:21:33 +0300

    Add files via upload

---
+++ Server/Waiter.java
@@ -0,0 +1,41 @@
+public class Waiter extends Employee {
+	private double cash;
+	private double pos_charges;
+	private double billing_account_charges;
+	public MobileDevice device;
+
+	public Waiter(double c, double pos_c, double bill_account, MobileDevice dev)
+	{
+		this.cash = c;
+		this.pos_charges = pos_c;
+		this.billing_account_charges = bill_account;
+		this.device = dev;
+
+	}
+
+	public static Waiter findBestForTable(Table t)
+	{
+ 		
+	}
+	
+	public boolean notify()
+	{
+		
+	}
+
+	public boolean isNew()
+	{
+		
+	}
+
+	public boolean editData(Bundle %%%%%)
+	{
+
+	}
+
+	public boolean validateData(Bundle $$$$)
+	{
+
+	}
+
+}
---
+++ Server/Waiting Group.java
@@ -0,0 +1,30 @@
+public class WaitingGroup {
+
+	private string name;
+	private int number_of_people;
+	private int priority;
+
+	public WaitingGroup(string n, int number_of_people, int priority)
+	{
+		this.name = n;
+		this.number_of_people = number_of_people;
+		this.priority = priority;
+
+	}
+
+
+	public void addToList()
+	{
+
+	}
+
+	public void notifyWhenAvailable()
+	{
+
+	}
+
+	public void remove()
+	{
+
+	}
+}
