b36317960d39910752d948556b8b59c4a46a19f1
rodoulampampa <32978419+rodoulampampa@users.noreply.github.com>
2019-05-09 12:54:39 +0300

    edit editData(), isNew() and validateData()
    In the function isNew() i am not sure if it's correct to call the function onPay() or will call in the main function.

--- Server/Waiter.java
+++ Server/Waiter.java
@@ -10,6 +10,8 @@ public class Waiter extends Employee {
 		this.pos_charges = pos_c;
 		this.billing_account_charges = bill_account;
 		this.device = dev;
+		
+		
 	}
 
 	public static Waiter findBestForTable(Table t)
@@ -21,22 +23,51 @@ public class Waiter extends Employee {
 	{
 		boolean res = n.show();
 		return res;
-	
 	}
 
-	public boolean isNew()
+	public boolean isNew(this)
 	{
+		if (this.pos_charges == !NULL)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+			void onPay();
+		}
 		
 	}
 
 	public boolean editData(Bundle new_info)
 	{
+		if (validateData(new_info))
+		{
+			return RESP_ACCEPTED;
+		}
+		else
+		{
+			return RESP_REJECTED;
+		}
 
 	}
 
 	public boolean validateData(Bundle new_info)
 	{
-
+		if ((this.new_info == this.username) && (this.new_info == this.password))
+		{
+			showSuccess("Your data is valid!");
+			//Waiter dismisses the message
+			return RESP_ACCEPTED;
+		}
+		else
+		{
+			showFailure("Your data is not valid");
+			//Waiter dismisses the message
+			return RESP_REJECTED;
+		
+		}
+		
 	}
 
 }
