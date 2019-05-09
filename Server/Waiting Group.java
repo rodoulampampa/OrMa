9fe9087240aeee870ed510c08fefe76d903a9bd9
rodoulampampa <32978419+rodoulampampa@users.noreply.github.com>
2019-05-09 13:30:41 +0300

    changes in the functions: addToList(), notifyWhenAvailable(),remove()
    need help in the function notifyWhenAvailable()

--- Server/Waiting Group.java
+++ Server/Waiting Group.java
@@ -1,8 +1,9 @@
 public class WaitingGroup {
 
 	private string name;
-	private int number_of_people;
+	private int number_of_people;	//perhaps to be a list
 	private int priority;
+	static List<WaitingGroup> WaitingGroup.name; //add to the domain model?
 
 	public WaitingGroup(string n, int number_of_people, int priority)
 	{
@@ -13,18 +14,46 @@ public class WaitingGroup {
 	}
 
 
-	public void addToList()
+	public void addToList(this.name, num_of_p)
 	{
-
+		for(WaitingGroup.name != this.name)
+		{
+			WaitingGroup.name = WaitingGroup.name + this.name;
+			this.number_of_people = this.number_of_people + this.num_of_p;
+		}
+		else
+		{
+			return false;
+		}
+		
+		return RESP_ACCEPTED;
+		notifyWhenAvailable();
 	}
 
-	public void notifyWhenAvailable()
+	public void notifyWhenAvailable(number_of_people)
 	{
-
+		if (table.findFreeTable() == NULL && (this.priority <= this.)) 
+			/////////////////need help here
+		return RESP_ACCEPTED;
+		else
+		{
+			return RESP_REJECTED;
+		}
 	}
 
-	public void remove()
+	public void remove(this.name, num_of_p)
 	{
-
+		if (setTaken == TRUE)
+		{
+			priority = priority -1;
+			WaitingGroup.name = WaitingGroup.name - this.name;
+			this.number_of_people = this.number_of_people - this.num_of_p;
+			return RESP_ACCEPTED;
+		}
+		else
+		{
+			return RESP_REJECTED;
+		}
+		
 	}
 }
