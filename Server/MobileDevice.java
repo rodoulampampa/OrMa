511d22f8843128d8b5ec83bbd05c66afc7a6c549
rodoulampampa <32978419+rodoulampampa@users.noreply.github.com>
2019-05-12 12:54:05 +0300

    Create the class Moblile Device

---
+++ Server/MobileDevice.java
@@ -0,0 +1,9 @@
+public class MobileDevice extends Device {
+    private List<Notification> notification_list;
+
+    public MobileDevice(List_notification notification_list) 
+    {
+            this.notification_list = notification_list;
+    }
+
+}
