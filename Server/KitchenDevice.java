e88c8b9dffedc33446d7faa12643115f57f198bc
anthoulab <49809899+anthoulab@users.noreply.github.com>
2019-05-16 13:41:24 +0300

    Update KitchenDevice.java

--- Server/KitchenDevice.java
+++ Server/KitchenDevice.java
@@ -1,12 +1,6 @@
 public class KitchenDevice extends Device {
 
-      private List<Order> order_list;
-      private List<Notification> notification_list;
-
-      public kitchenDevice(List<Order> order_list, List<Notification> notification_list) {
-            this.order_list = order_list;
-            this.notification_list = notification_list;
-      }
-
+      private List<Order> order_list = new ArrayList<Order>();
+      private List<Notification> notification_list = new ArrayList<Notification>();
 
 }
